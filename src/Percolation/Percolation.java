3b70ade87b61addd3549858384717345c7def67a
JinghongYang <jinghong.yang82@gmail.com>
2016-02-24 17:44:52 +0100

    Percolation API

--- src/Percolation/Percolation.java
+++ src/Percolation/Percolation.java
@@ -1,5 +1,13 @@
-package Percolation;
+/*********************************************
+ * We model a percolation system using an N-by-N grid of sites. Each site is either open or blocked. 
+ * A full site is an open site that can be connected to an open site in the top row via a chain of 
+ * neighboring (left, right, up, down) open sites. We say the system percolates if we fill all open 
+ * sites connected to the top row and that process fills some open site on the bottom row. 
+ * 
+ *  @author Jinghong Yang
+ *******************************************************************
 
+package Percolation;
 
 import edu.princeton.cs.algs4.WeightedQuickUnionUF;
 
