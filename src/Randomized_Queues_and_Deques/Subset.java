f15e277058c37059a4891519b3262d0f15b65174
JinghongYang <jinghong.yang82@gmail.com>
2016-02-24 18:08:31 +0100

    Update Subset.java

--- src/Randomized_Queues_and_Deques/Subset.java
+++ src/Randomized_Queues_and_Deques/Subset.java
@@ -1,3 +1,15 @@
+/*************************************************
+ * This program takes a command-line integer k; 
+ * reads in a sequence of N strings from standard input using StdIn.readString(); 
+ * and prints out exactly k of them, uniformly at random. 
+ * Each item from the sequence can be printed out at most once. 
+ * We assume that 0 ≤ k ≤ N, where N is the number of string on standard input.
+ * 
+ * @author Jinghong Yang
+ * 
+ * ****************************************************************
+
+
 package Randomized_Queues_and_Deques;
 import edu.princeton.cs.algs4.StdIn;
 import edu.princeton.cs.algs4.StdOut;
