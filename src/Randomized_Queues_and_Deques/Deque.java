78bd2af317970b9aee62a5d9704c741502bce4b2
JinghongYang <jinghong.yang82@gmail.com>
2016-02-24 18:03:08 +0100

    Deque API

--- src/Randomized_Queues_and_Deques/Deque.java
+++ src/Randomized_Queues_and_Deques/Deque.java
@@ -1,3 +1,12 @@
+/***********************************************
+ * A double-ended queue or deque (pronounced "deck") is a generalization 
+ * of a stack and a queue that supports adding and removing items from 
+ * either the front or the back of the data structure. 
+ * 
+ * @author Jinghong Yang
+ * 
+ * *****************************************************
+ 
 package Randomized_Queues_and_Deques;
 import java.util.Iterator;
 
